7e43942ed379bb9fe767140761f337c2856d3e43
Yung-Yu Chen <yyc@solvcon.net>
2018-06-18 06:13:27 +0800

    Add cmake build script.

---
+++ bin/admin/build-install-cmake.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#
+# Copyright (C) 2011 Yung-Yu Chen <yyc@solvcon.net>.
+
+cmake_vminor=3.11
+cmake_vpatch=3
+cmake_dn=cmake-$cmake_vminor.$cmake_vpatch
+cmake_fn=$cmake_dn.tar.gz
+cmake_url=https://cmake.org/files/v$cmake_vminor/$cmake_fn
+
+mkdir -p ~/tmp
+cd ~/tmp
+rm -rf $cmake_dn $cmake_fn
+wget -q $cmake_url
+tar xf $cmake_fn
+
+cd $cmake_dn
+{ time ./configure --prefix=$HOME/opt/cmake ; } > configure.log 2>&1
+{ time make -j $NP ; } > make.log 2>&1
+{ time make install ; } > install.log 2>&1
